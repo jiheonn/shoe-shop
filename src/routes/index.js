4f612985e50662917db30143b8395dde49802a53
Jiheon-Lee <wlgjsdl0@gmail.com>
2021-08-20 02:12:16 +0900

    Add index render

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
-import express from "express";
-const router = express.Router();
+import express from 'express'
+
+const router = express.Router()
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
-});
+router.get('/', (req, res) => {
+  res.render('index')
+})
 
-export default router;
+export default router
