d1175018cb72c795b35a630c8678fc35e57db0ba
Jiheon-Lee <wlgjsdl0@gmail.com>
2021-08-22 18:07:38 +0900

    Add main page Contents

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,24 @@
 import express from 'express'
+import db_querys from '../db/querys'
+import formater from '../format/formater'
 
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const username = req.user ? req.user.u_name : ''
 
-  res.render('index', { username })
+  const latestedShoes6 = await db_querys.selectLatestLimit6()
+  const popularShoes6 = await db_querys.selectPopularLimit6()
+
+  const brandList = await db_querys.selectBrandList()
+
+  res.render('index', {
+    username,
+    latestedShoes6: formater.productInfoFormat(latestedShoes6),
+    popularShoes6: formater.productInfoFormat(popularShoes6),
+    brandList,
+  })
 })
 
 export default router
