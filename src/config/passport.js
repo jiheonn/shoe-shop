e7d9f618870e54fcc8361447c95485dc50aa4f79
jiheonn <jiheon.lee.dev@gmail.com>
2021-09-19 15:19:45 +0900

    feat: Add exceoption handling

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,13 +11,17 @@ export default app => {
     done(null, user.u_id) // deserializeUser로 user 전달
   })
   // 로그인에 성공하고, 페이지를 방문할 때마다 호출
-  passport.deserializeUser(async (u_id, done) => {
-    const user = await db_querys.selectUserInfo(u_id)
-
-    done(null, user) // req.user 객체 생성
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await db_querys.selectUserInfo(id)
+      done(null, user) // req.user 객체 생성
+    } catch {
+      done(null, false, {
+        message: '서버의 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+      })
+    }
   })
 
-  // Local
   passport.use(
     new LocalStrategy(
       {
@@ -26,20 +30,26 @@ export default app => {
         passwordField: 'pw',
       },
       async (id, pw, done) => {
-        // 회원정보 조회
-        const user = await db_querys.selectUserInfo(id)
+        try {
+          // 회원정보 조회
+          const user = await db_querys.selectUserInfo(id)
 
-        // 회원정보가 없는 경우
-        if (!user) {
-          return done(null, false, { message: '존재하지 않는 아이디입니다.' })
-        }
-        // 비밀번호가 일치하지 않는 경우
-        if (user.u_pw !== pw) {
-          return done(null, false, {
-            message: '비밀번호가 일치하지 않습니다.',
+          // 회원정보가 없는 경우
+          if (!user) {
+            done(null, false, { message: '존재하지 않는 아이디입니다.' })
+          }
+          // 비밀번호가 일치하지 않는 경우
+          if (user.u_pw !== pw) {
+            done(null, false, {
+              message: '비밀번호가 일치하지 않습니다.',
+            })
+          }
+          done(null, user)
+        } catch {
+          done(null, false, {
+            message: '서버의 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.',
           })
         }
-        return done(null, user)
       }
     )
   )
