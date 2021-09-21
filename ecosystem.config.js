4670c76df26d6aa1147b8ea9eccc9172775bcee4
jiheonn <jiheon.lee.dev@gmail.com>
2021-09-21 15:48:04 +0900

    chore: Update cluster instances option

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'app',
       script: './dist/bin/www.js',
-      instances: 4,
+      instances: 0,
       exec_mode: 'cluster',
       wait_ready: true,
       listen_timeout: 50000,
