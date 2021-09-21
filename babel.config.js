8395e1f5d53a5da8acb002d7b465ae5ba4c12994
jiheonn <jiheon.lee.dev@gmail.com>
2021-09-21 15:50:08 +0900

    chore: Delete babel & Add typescript

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['@babel/env'],
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? ['transform-remove-console', '@babel/plugin-transform-runtime']
-      : ['@babel/plugin-transform-runtime'],
-}
