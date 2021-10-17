4f05797b95097e516b6d5fe28d739b1709608d98
jiheonn <jiheon.lee.dev@gmail.com>
2021-10-17 14:00:09 +0900

    chore: Update auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,8 +1,18 @@
-const displayLogin = (req, res) => {
+import * as express from 'express'
+
+// TODO: types 분리 필요
+declare module 'express' {
+  export interface Request {
+    flash: any
+    session: any
+  }
+}
+
+const displayLogin = (req: express.Request, res: express.Response) => {
   const message = req.flash('error')
   res.render('login', { message })
 }
-const logout = (req, res) => {
+const logout = (req: express.Request, res: express.Response) => {
   req.session.destroy()
   res.redirect('/')
 }
