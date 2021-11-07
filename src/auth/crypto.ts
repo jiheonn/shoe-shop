e5c01e7d59ad3f94af5d71e9d3c3081d7fa04120
jiheonn <jiheon.lee.dev@gmail.com>
2021-11-07 14:31:04 +0900

    feat: Add user password createSalt, compare function

--- src/auth/crypto.ts
+++ src/auth/crypto.ts
@@ -1,16 +1,36 @@
 import * as util from 'util'
 import * as crypto from 'crypto'
 
+import { User } from '../db/models'
+
 const randomBytesPromise = util.promisify(crypto.randomBytes)
 const pbkdf2Promise = util.promisify(crypto.pbkdf2)
 
-const createHashedPassword = async password => {
+const createSalt = async () => {
   const buf = await randomBytesPromise(64)
-  const salt = buf.toString('base64')
+
+  return buf.toString('base64')
+}
+
+export const createHashedPassword = async password => {
+  const salt = await createSalt()
+
   const key = await pbkdf2Promise(password, salt, 99999, 64, 'sha512')
   const hashedPassword = key.toString('base64')
 
   return { hashedPassword, salt }
 }
 
-export default createHashedPassword
+export const compare = async (userId, password) => {
+  const salt = await User.findOne({
+    attributes: ['salt'],
+    where: {
+      id: userId,
+    },
+    raw: true,
+  }).then(result => result.salt)
+
+  const key = await pbkdf2Promise(password, salt, 99999, 64, 'sha512')
+
+  return key.toString('base64')
+}
