bf7cf818a884db9bf35371b7c29e40a3e1a4a545
jiheonn <jiheon.lee.dev@gmail.com>
2021-11-08 15:31:17 +0900

    feat: Add user password verifyPassword logic & comment

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -2,6 +2,7 @@ import * as passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 
 import { User } from '../db/models'
+import { verifyPassword } from './crypto'
 
 // TODO: types 분리 필요
 declare module 'express' {
@@ -14,14 +15,16 @@ export default app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  // 로그인 성공했을 때 한 번만 실행
+  // (2) - 로그인 인증 성공 시 한 번만 실행
   passport.serializeUser((user, done) => {
-    done(null, user.id) // deserializeUser로 user 전달
+    done(null, user.id) // 세션에 사용자 정보인 id를 저장
   })
-  // 로그인에 성공하고, 페이지를 방문할 때마다 호출
+  // (3) - 로그인 인증이 되어있는 경우, 요청할 때마다 실행
   passport.deserializeUser(async (id, done) => {
+    // 세션에 저장되어 있는 id를 인자 값으로 전달받음
     try {
       const user = await User.findOne({
+        attributes: ['id', 'email', 'name'],
         where: {
           id,
         },
@@ -35,11 +38,12 @@ export default app => {
     }
   })
 
+  // (1)
   passport.use(
     new LocalStrategy(
       {
-        session: true,
-        usernameField: 'id', // form > input name 값
+        session: true, // 세션 저장 여부
+        usernameField: 'id', // form > input name
         passwordField: 'password',
       },
       async (id, password, done) => {
@@ -52,15 +56,25 @@ export default app => {
             raw: true,
           })
 
-          // 회원정보가 없거나 비밀번호가 일치하지 않는 경우
-          if (!user || user.password !== password) {
+          // 회원정보가 없는 경우
+          if (!user) {
             done(null, false, {
-              message:
-                '존재하지 않는 아이디이거나 비밀번호가 일치하지 않습니다.',
+              message: '존재하지 않는 아이디입니다.',
             })
-          } else {
-            done(null, user)
           }
+
+          const verified = await verifyPassword(
+            password,
+            user.salt,
+            user.password,
+          )
+          // 비밀번호가 일치하지 않는 경우
+          if (!verified) {
+            done(null, false, {
+              message: '비밀번호가 일치하지 않습니다.',
+            })
+          }
+          done(null, user) // serializeUser로 user 전달
         } catch {
           done(null, false, {
             message: '서버의 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.',
